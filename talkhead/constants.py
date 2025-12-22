b97916b3fe8f00f7bce4018b7a8adf87a8eb0e2c
aerast <farbitherast5888@gmail.com>
2025-12-22 00:06:29 +0000

    fix: filter out invalid responses

--- talkhead/constants.py
+++ talkhead/constants.py
@@ -4,10 +4,10 @@ dotenv.load_dotenv()
 
 TALKHEAD_SERVER_HOST = "https://subnet.talkhead.ai"
 
-SCORING_SERVER_ENDPOINT=os.getenv("SCORING_SERVER_ENDPOINT", "http://localhost:8100/score")
+SCORING_SERVER_ENDPOINT=os.getenv("SCORING_SERVER_ENDPOINT", f"{TALKHEAD_SERVER_HOST}/score")
 BURN_UID = int(os.getenv("BURN_UID", 0))
 BURN_RATIO = float(os.getenv("BURN_RATIO", 1.0))
 
 MINER_SERVER_ENDPOINT=os.getenv("MINER_SERVER_ENDPOINT", "http://localhost:9000/generate")
 
-DENDRITE_TIMEOUT = 120
+DENDRITE_TIMEOUT = int(os.getenv("DENDRITE_TIMEOUT", 120))
